94eb0a8db568b20d3f1464751b3fdf5e78580bbb
victoriahalim <vhalim@deloitte.com.au>
2024-08-20 14:07:37 +1000

    childlisting: config

---
+++ blocks/childlisting/childlisting.js
@@ -0,0 +1,28 @@
+export default function decorate(block) {
+  const allDivs = block.querySelectorAll("div");
+
+  allDivs.forEach(function (div) {
+    div.classList.add("childlisting-details");
+  });
+
+  const listings = block.querySelectorAll("p");
+  listings.forEach(function (listing) {
+    listing.classList.remove("button-container");
+  });
+
+  const anchors = block.querySelectorAll("a");
+  anchors.forEach(function (anchor) {
+    anchor.classList.remove("button");
+  });
+
+  /*
+  allDivs.forEach(function(div){
+      div.classList.remove('button-container');
+  });*/
+
+  /*
+  const linksDiv = block.querySelector(':scope>div>div>div>div>');
+  const links = document.createElement("p")
+  links.classList.add("linksstyling");
+  linksDiv.replacewith(links);*/
+}
